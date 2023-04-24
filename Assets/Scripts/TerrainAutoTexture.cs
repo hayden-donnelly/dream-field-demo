e11bcca82692630fe644fb803b862e1989551449
hayden-donnelly <donnellyhd@outlook.com>
2023-04-24 18:39:11 -0400

    Auto textue with scriptableobject settings.

--- Assets/Scripts/TerrainAutoTexture.cs
+++ Assets/Scripts/TerrainAutoTexture.cs
@@ -2,22 +2,9 @@
 
 public class TerrainAutoTexture : MonoBehaviour
 {
-    // The terrain to texture
     public Terrain terrain;
+    [SerializeField] private TerrainAutoTextureScriptableObject settings;
 
-    // The textures to apply to the terrain based on height and steepness ranges
-    public Texture2D[] textures;
-
-    // The height ranges for each texture (0 to 1)
-    public float[] heightRanges;
-
-    // The steepness ranges for each texture (0 to 90)
-    public float[] steepnessRanges;
-
-    // The splatmap resolution of the terrain
-    public int splatmapResolution = 512;
-
-    // The method to apply the textures to the terrain
     public void ApplyTextures()
     {
         // Get the terrain data
@@ -32,16 +19,16 @@ public class TerrainAutoTexture : MonoBehaviour
         float length = terrainData.size.z;
 
         // Create a new splatmap array
-        float[,,] splatmap = new float[splatmapResolution, splatmapResolution, textures.Length];
+        float[,,] splatmap = new float[settings.splatmapResolution, settings.splatmapResolution, settings.numTextures];
 
         // Loop through each pixel of the splatmap
-        for (int i = 0; i < splatmapResolution; i++)
+        for (int i = 0; i < settings.splatmapResolution; i++)
         {
-            for (int j = 0; j < splatmapResolution; j++)
+            for (int j = 0; j < settings.splatmapResolution; j++)
             {
                 // Get the normalized coordinates of the pixel
-                float x = (float)i / (float)splatmapResolution;
-                float y = (float)j / (float)splatmapResolution;
+                float x = (float)i / (float)settings.splatmapResolution;
+                float y = (float)j / (float)settings.splatmapResolution;
 
                 // Get the interpolated height at this point
                 float height = heightmap[(int)(x * heightmapResolution), (int)(y * heightmapResolution)];
@@ -51,21 +38,13 @@ public class TerrainAutoTexture : MonoBehaviour
                 float steepness = terrainData.GetSteepness(x, y);
 
                 // Loop through each texture
-                for (int k = 0; k < textures.Length; k++)
+                for (int k = 0; k < settings.numTextures; k++)
                 {
-                    // Check if the height and steepness are within the ranges for this texture
-                    //if (height >= heightRanges[k] && height < heightRanges[k + 1] && steepness >= steepnessRanges[k] && steepness < steepnessRanges[k + 1])
-                    //if (height >= heightRanges[k] && height < heightRanges[k + 1])
-                    if(steepness >= steepnessRanges[k] && steepness < steepnessRanges[k + 1])
-                    {
-                        // Set the splatmap value to 1 for this texture
-                        splatmap[i, j, k] = 1f;
-                    }
-                    else
-                    {
-                        // Set the splatmap value to 0 for this texture
-                        splatmap[i, j, k] = 0f;
-                    }
+                    float steepnessRange = settings.steepnessRanges[k + 1] - settings.steepnessRanges[k];
+                    float steepnessValue = steepness - settings.steepnessRanges[k];
+                    float steepnessPercent = steepnessValue / steepnessRange;
+                    float splatmapValue = settings.blendingCurve.Evaluate(steepnessPercent);
+                    splatmap[i, j, k] = splatmapValue;
                 }
             }
         }
---
+++ Assets/Scripts/TerrainAutoTextureScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TerrainAutoTextureSettings", menuName = "ScriptableObjects/TerrainAutoTextureSettings", order = 1)]
+public class TerrainAutoTextureSettings : ScriptableObject
+{
+    public AnimationCurve curve;
+}
