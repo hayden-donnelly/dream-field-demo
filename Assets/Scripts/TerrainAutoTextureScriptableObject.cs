5083423260b961c9e474c324ba0cc547c04dd8d2
hayden-donnelly <donnellyhd@outlook.com>
2023-07-03 23:27:53 -0400

    Pivoted to dream field demo.

--- Assets/Scripts/TerrainAutoTextureScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TerrainAutoTextureSettings", menuName = "ScriptableObjects/TerrainAutoTextureSettings", order = 1)]
-public class TerrainAutoTextureSettings : ScriptableObject
-{
-    public AnimationCurve curve;
-}
