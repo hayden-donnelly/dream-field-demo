275efb61c08cfc5c3e82a9c7236817b7dd2e9c90
hayden-donnelly <donnellyhd@outlook.com>
2023-07-13 21:25:57 -0400

    Smooth and snap turn.

--- Assets/Scripts/MyController.cs
+++ Assets/Scripts/MyController.cs
@@ -16,9 +16,21 @@ namespace LAI.XR
         [SerializeField] private InputActionAsset actionAsset;
         private const string leftHandName = "LeftHand";
         private const string rightHandName = "RightHand";
+        [SerializeField] private GameObject origin;
+        private Transform originTransform;
+        [Header("Turning")]
+        [SerializeField] private bool snapTurn = true;
+        [SerializeField] private float snapTurnAngle = 45f;
+        [SerializeField] private float snapTurnCooldown = 0.5f;
+        private bool canSnapTurn = true;
+        [SerializeField] private float smoothTurnSpeed = 60f;
+        private bool isRotating = false;
+        private Vector2 rotationInput;
 
         private void Start()
         {
+            originTransform = origin.transform;
+
             string controllerName = 
                 (controllerType == ControllerType.Left) ? leftHandName : rightHandName;
             InputActionMap handBase = actionAsset.FindActionMap("XRI " + controllerName);
@@ -27,23 +39,71 @@ namespace LAI.XR
             InputAction positionAction = handBase.FindAction("Position");
             positionAction.performed += context => 
             { 
-                UpdatePosition(context.ReadValue<Vector3>()); 
+                UpdateHandPosition(context.ReadValue<Vector3>()); 
             };
             InputAction rotationAction = handBase.FindAction("Rotation");
             rotationAction.performed += context => 
             { 
-                UpdateRotation(context.ReadValue<Quaternion>()); 
+                UpdateHandRotation(context.ReadValue<Quaternion>()); 
             };
+
+            InputActionMap locomotion = 
+                actionAsset.FindActionMap("XRI " + controllerName + " Locomotion");
+            locomotion.Enable();
+            if(controllerType == ControllerType.Right)
+            {
+                InputAction turn = locomotion.FindAction("Turn");
+                turn.performed += context => 
+                { 
+                    rotationInput = context.ReadValue<Vector2>();
+                    isRotating = true;
+                };
+                turn.canceled += context => 
+                { 
+                    rotationInput = Vector2.zero;
+                    isRotating = false;
+                };
+            }
         }
 
-        private void UpdatePosition(Vector3 position)
+        private void Update()
+        {
+            if(isRotating)
+            {
+                if(snapTurn) { SnapRotateBody(rotationInput); }
+                else { SmoothRotateBody(rotationInput); }
+            }
+        }
+
+        private void UpdateHandPosition(Vector3 position)
         {
             transform.position = position;
         }
 
-        private void UpdateRotation(Quaternion rotation)
+        private void UpdateHandRotation(Quaternion rotation)
         {
             transform.rotation = rotation;
+        }
+
+        private void SnapRotateBody(Vector2 rotationInput)   
+        {
+            if(!canSnapTurn) { return; }
+            if(rotationInput.x > 0) { originTransform.Rotate(0, snapTurnAngle, 0); }
+            else if(rotationInput.x < 0) { originTransform.Rotate(0, -snapTurnAngle, 0); }
+            StartCoroutine(SnapTurnCooldown());
+        }
+
+        private IEnumerator SnapTurnCooldown()
+        {
+            canSnapTurn = false;
+            yield return new WaitForSeconds(snapTurnCooldown);
+            canSnapTurn = true;
+        }
+
+        private void SmoothRotateBody(Vector2 rotationInput)   
+        {
+            float yRotation = rotationInput.x * smoothTurnSpeed * Time.deltaTime;
+            originTransform.Rotate(0, yRotation, 0);
         }
     }
 }
